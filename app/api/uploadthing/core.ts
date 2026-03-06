ff7159d597938007a4acba74a146d8c391c54063
19Vako <152034949+19Vako@users.noreply.github.com>
2026-03-06 20:08:22 +0200

    add: uploadthing

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -3,16 +3,13 @@ import { createUploadthing, type FileRouter } from "uploadthing/next";
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  // Определяем маршрут для загрузки фото комнаты
+
   roomImage: f({ image: { maxFileSize: "16MB", maxFileCount: 5 } })
     .middleware(async ({ req }) => {
-      // Здесь позже можно добавить проверку сессии на АДМИНА
-      // const session = await auth();
-      // if (session?.user?.role !== "ADMIN") throw new Error("Unauthorized");
       return { }; 
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      // Этот код сработает на сервере, когда картинка загрузится
+   
       console.log("File URL: ", file.url);
       return { url: file.url };
     }),
