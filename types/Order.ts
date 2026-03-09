2bd77f2ad67106fafc26c2ade46cf4c7de9b43ee
19Vako <152034949+19Vako@users.noreply.github.com>
2026-03-09 23:07:21 +0200

    update: types

--- types/Order.ts
+++ types/Order.ts
@@ -1,6 +1,7 @@
 import { ObjectId } from "mongodb";
 export default interface Order {
     userId: ObjectId | string,
+    numberOfPeople: number,
     roomId: ObjectId | string,
     price: number,
     checkInDate: Date,
