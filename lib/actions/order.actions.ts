1d322dd22c513c8f7f58a983e8e1460ea3f1e069
19Vako <152034949+19Vako@users.noreply.github.com>
2026-03-11 22:24:06 +0200

    add: getAvailableRooms

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -5,10 +5,9 @@ import { ObjectId } from "mongodb"
 import User from "@/types/User"
 import { auth } from "@/auth"
 import { revalidatePath } from "next/cache"
+import Order from "@/types/Order"
+import Room from "@/types/Room"
 
-const client = await clientPromise
-const db = client.db(process.env.DB_NAME)
-const session = await auth()
 
 
 export async function createOrder(
@@ -17,8 +16,10 @@ export async function createOrder(
     checkOutDate: Date, 
     price: number,
     numberOfPeople: number,
-) {
-    
+): Promise<{ success: boolean, error?: string }> {
+    const client = await clientPromise
+    const db = client.db(process.env.DB_NAME)
+    const session = await auth()
     if (!session?.user?.id) {
         return { success: false, error: "Please log in to the system" }
     }
@@ -60,7 +61,7 @@ export async function createOrder(
         )
 
         revalidatePath("/")  
-        return { success: true, orderId: result.insertedId }
+        return { success: true}
 
     } catch (error) {
         console.error("Database Error:", error)
@@ -68,7 +69,54 @@ export async function createOrder(
     }
 }
 
-export async function cancelOrder(orderId: string) {
+export async function getAvailableRooms(checkInDate: Date, checkOutDate: Date, numberOfPeople: number): Promise<{ success: boolean, rooms?: Room[], error?: string }> {
+    const client = await clientPromise
+    const db = client.db(process.env.DB_NAME)
+    try {
+
+    const checkIn = new Date(checkInDate);
+    const checkOut = new Date(checkOutDate);
+
+
+    const overlappingOrders = await db.collection<Order>("orders").find({
+      status: { $in: ["PENDING", "CONFIRMED"] },
+      $and: [
+        { checkInDate: { $lt: checkOut } }, 
+        { checkOutDate: { $gt: checkIn } }  
+      ]
+    }).project({ roomId: 1 }).toArray();
+
+ 
+    const bookedRoomIds = overlappingOrders.map(order => order.roomId);
+
+    const rawRooms = await db.collection("rooms").find({
+      _id: { $nin: bookedRoomIds },
+      capacity: { $gte: numberOfPeople }
+    }).toArray();
+
+    const availableRooms: Room[] = rawRooms.map(room => ({
+      id: room._id.toString(), 
+      roomName: room.roomName,
+      type: room.type,  
+      price: room.price,
+      capacity: room.capacity,
+      photoUrl: room.photoUrl,
+      status: room.status,
+    }));
+
+
+    return { success: true, rooms: JSON.parse(JSON.stringify(availableRooms)) };
+
+  } catch (error) {
+    console.error("Database Error:", error)
+    return { success: false, error: "Something went wrong" }
+  }
+}
+
+export async function cancelOrder(orderId: string): Promise<{ success: boolean, error?: string }> {
+    const client = await clientPromise
+    const db = client.db(process.env.DB_NAME)
+    const session = await auth()
     if (!session?.user?.id) {
         return { success: false, error: "Please log in to the system" }
     }
