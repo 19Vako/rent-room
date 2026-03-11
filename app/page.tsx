e514d65ca3c01d1239fb2a658a12259f7d95afb1
19Vako <152034949+19Vako@users.noreply.github.com>
2026-03-11 22:23:22 +0200

    update: app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -30,13 +30,13 @@ export default function Home() {
         </div>
       </section>
 
-      {/* Место для списка комнат */}
+   {/* 
       <section className="max-w-5xl mx-auto px-4 py-12">
         <h2 className="text-2xl font-bold mb-6 text-gray-800">Доступные варианты</h2>
         <div className="text-gray-500">
           Здесь скоро появятся карточки комнат из базы данных...
         </div>
-      </section>
+      </section> */}
 
     </main>
   );
