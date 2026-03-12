f0b84461103b781ad85bc5ac1801287cfe9e22b5
19Vako <152034949+19Vako@users.noreply.github.com>
2026-03-12 21:23:20 +0200

    update: admin/page.tsx

---
+++ app/admin/page.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/shared/Header";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      
+      <Header />
+
+    </main>
+  );
+}
