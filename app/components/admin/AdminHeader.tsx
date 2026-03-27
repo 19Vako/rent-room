2a41f999d3fb7125b8f275aa53d618495f4b7644
19Vako <152034949+19Vako@users.noreply.github.com>
2026-03-27 12:41:12 +0200

    feat: add admin/AdminHeader.tsx

---
+++ app/components/admin/AdminHeader.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { getAllRooms } from "@/lib/actions/room.actions";  
+import AdminRoomSelector from "./AdminRoomSelector";
+
+export default async function AdminHeader() {
+ 
+  const { success, rooms } = await getAllRooms();
+
+  return (
+    <header className="bg-[#003580] border-b border-[#002255] z-50 relative">
+      <div className="max-w-[1400px] mx-auto px-4 h-16 flex items-center justify-between">
+        
+        <div className="flex items-center gap-6">
+ 
+          <Link href="/admin" className="text-2xl font-bold text-white tracking-tight">
+             RentRoom
+          </Link>
+
+          {success && rooms && rooms.length > 0 && (
+            <AdminRoomSelector rooms={rooms} />
+          )}
+        </div>
+
+    
+      </div>
+    </header>
+  );
+}
